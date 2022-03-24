eece6ffeb9be96ff0c790f38f26c429b01e0adbd
Kevin Stechler <kevinstechler@WTMBP-Kevin-Stechler.local>
2022-03-24 11:28:31 -0400

    initial commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Kevin Stechler on 3/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/TimerIntervalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerIntervalApp.swift
+//  Shared
+//
+//  Created by Kevin Stechler on 3/24/22.
+//
+
+import SwiftUI
+
+@main
+struct TimerIntervalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
